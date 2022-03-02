ba7637c29795e0b92fcfb6e17a3eca5b8493f2f6
hys <nemona_01@naver.com>
2022-03-02 17:38:44 +0900

    초기 커밋.

---
+++ cSharp_Tetris2/Block.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_Tetris2
+{
+    internal class Block
+    {
+
+        private static Block[] blockArray;
+
+        public int Width;
+        public int Height;
+        public int[,] Dots;
+
+        static Block()
+        {
+            blockArray = new Block[]
+            {
+               new Block
+               {
+                   Width = 3,
+                   Height = 2,
+                   Dots = new int[2, 3]
+                   {
+                       {1, 1, 0},
+                       {0, 1, 1}
+                   }
+               },
+
+               new Block
+               {
+                   Width = 2,
+                   Height = 2,
+                   Dots = new int[2, 2]
+                   {
+                       {1, 1},
+                       {1, 1}
+                   }
+               },
+
+               new Block
+               {
+                   Width = 1,
+                   Height = 4,
+                   Dots = new int [4, 1]
+                   {
+                       {1},
+                       {1},
+                       {1},
+                       {1}
+                   }
+               },
+
+               new Block
+               {
+                   Width = 2,
+                   Height = 3,
+                   Dots = new int[3, 2]
+                   {
+                       {1, 1 },
+                       {1, 0 },
+                       {1, 0 }
+                   }
+               }
+            };
+
+        }
+
+        public static Block GetRandomShape()
+        {
+            var block = blockArray[new Random().Next(blockArray.Length)];
+
+            return block;
+        }
+    }
+}
---
+++ cSharp_Tetris2/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cSharp_Tetris2
+{
+    public partial class Form1 : Form
+    {
+        Block currentBlock;
+        Timer timer = new Timer();
+        int currentX;
+        int currentY;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            boardLoad();
+
+            currentBlock = getRandomBlock();
+
+            timer.Interval = 500;
+            timer.Tick += timer1_Tick;
+            timer.Start();
+
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            var verticalMove = 0;
+            var horizontalMove = 0;
+
+            switch (e.KeyCode)
+            {
+
+                case Keys.A:
+                    verticalMove -= 1;
+                    break;
+
+                case Keys.D:
+                    verticalMove += 1;
+                    break;
+
+                case Keys.S:
+                    horizontalMove++;
+                    break;
+
+                case Keys.W:
+                    Console.WriteLine("turn");
+                    break;
+
+                default:
+                    return;
+            }
+
+            moveBlock(horizontalMove, verticalMove);
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveBlock(moveDown: 1);
+            check();
+            
+        }
+
+        private void moveBlock(int moveDown = 0, int moveSide = 0)
+        {
+            var newX = currentX + moveSide;
+            var newY = currentY + moveDown;
+
+            currentX = newX;
+            currentY = newY;
+
+            drawShape();
+        }
+
+        private void check()
+        {
+        
+        }
+
+
+        private Block getRandomBlock()
+        {
+            var block = Block.GetRandomShape();
+
+            currentX = 7;
+            currentY = -block.Height;
+
+            return block;
+        }
+
+        Bitmap boardBitmap;
+        Graphics boardGraphics;
+
+        int boardWidth = 15;
+        int boardHeight = 20;
+        int dotSize = 20;
+
+        private void boardLoad()
+        {
+            pictureBox1.Width = boardWidth * dotSize;
+            pictureBox1.Height = boardHeight * dotSize;
+
+            boardBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            boardGraphics = Graphics.FromImage(boardBitmap);
+
+            boardGraphics.FillRectangle(Brushes.LightGray, 0, 0, boardBitmap.Width, boardBitmap.Height);
+
+            pictureBox1.Image = boardBitmap;
+        }
+
+
+        Bitmap workingBitmap;
+        Graphics workingGraphics;
+
+        private void drawShape()
+        {
+            workingBitmap = new Bitmap(boardBitmap);
+            workingGraphics = Graphics.FromImage(workingBitmap);
+
+            for (int i = 0; i < currentBlock.Width; i++)
+            {
+                for (int j = 0; j < currentBlock.Height; j++)
+                {
+                    if (currentBlock.Dots[j, i] == 1)
+                        workingGraphics.FillRectangle(Brushes.Red, (currentX + i) * dotSize,  (currentY + j) * dotSize, dotSize, dotSize);
+                }
+            }
+
+            pictureBox1.Image = workingBitmap;
+
+        }
+
+        
+    }
+}
